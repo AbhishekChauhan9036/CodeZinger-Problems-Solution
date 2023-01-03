d52d9527ac5d7058f0ad24596659c67cd19eb50c
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-01-03 13:02:15 +0530

    3 january Done

---
+++ 03-January-2022/count-array-element-that-divide-sum-of-all-other-element.js
@@ -0,0 +1,54 @@
+/*--------------------------------------------------------------------------------------------------------------------------*/
+/* Author = Chauhan Abhishek */
+/* Codechef Id = https://www.codechef.com/users/abhishek_9036 */
+/* LeetCode Id = https://leetcode.com/abhishekchauhan9036/ */
+/* GeeksForGeeks Id = https://auth.geeksforgeeks.org/user/abhishekchauhan9036/profile */
+/* GitHub Id = https://github.com/AbhishekChauhan9036 */
+/* Language = Node.Js */
+/* Address = Ballia,UP,INDIA (221716)*/
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+function solution(arr,n){
+    let sum=0;
+   let cnt=0;
+   for(let i=0;i<n;i++){
+      sum+=arr[i];
+   }
+   
+   for(let i=0;i<n;i++){
+      let minus=sum-arr[i];
+      if(minus%arr[i]==0){
+         cnt++;
+      }
+   }
+   return cnt;
+}
+function main() {
+    var t = 1;
+    // t = parseInt(readLine());
+    while (t--) {
+        var n = parseInt(readLine());
+        var arr = readLine().split(" ").map(Number);
+        var res = solution(arr,n);
+        console.log(res);
+    }
+}
+
